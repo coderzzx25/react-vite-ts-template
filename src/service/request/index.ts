4b62c0aa3330d82b32140be54e3ddc987771b74b
coderzzx <coderzzx@coderzzxMacBook-Air.local>
2025-04-23 11:26:27 +0800

    Mod -> service -> 更新网络请求

--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -1,77 +1,65 @@
 import axios from 'axios';
-import type { AxiosInstance } from 'axios';
-import type { RequestConfig } from './type';
+import type { AxiosInstance, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
+import type { Interceptors, RequestConfig } from './type';
 
 class Request {
-  instance: AxiosInstance;
+  private instance: AxiosInstance;
+  private interceptors?: Interceptors;
 
-  // request实例 => axios的实例
   constructor(config: RequestConfig) {
     this.instance = axios.create(config);
+    this.interceptors = config.interceptors;
 
-    // 每个instance实例都添加拦截器
+    // 用户自定义拦截器
+    if (this.interceptors) {
+      const { requestFailureFn, requestSuccessFn, responseFailureFn, responseSuccessFn } = this.interceptors;
+
+      requestFailureFn && this.instance.interceptors.request.use(requestSuccessFn, requestFailureFn);
+      responseFailureFn && this.instance.interceptors.response.use(responseSuccessFn, responseFailureFn);
+    }
+
+    // 默认拦截器：loading
     this.instance.interceptors.request.use(
-      (config) => {
+      (config: InternalAxiosRequestConfig) => {
         return config;
       },
-      (err) => {
-        return Promise.reject(err);
-      }
-    );
-    this.instance.interceptors.response.use(
-      (res) => {
-        return Promise.resolve(res);
-      },
-      (err) => {
-        return Promise.reject(err);
+      (error) => {
+        return Promise.reject(error);
       }
     );
 
-    // 针对特定的Request实例添加拦截器
-    this.instance.interceptors.request.use(
-      config.interceptors?.requestSuccessFn,
-      config.interceptors?.requestFailureFn
-    );
     this.instance.interceptors.response.use(
-      config.interceptors?.responseSuccessFn,
-      config.interceptors?.responseFailureFn
+      (res: AxiosResponse) => {
+        return res;
+      },
+      (error) => {
+        return Promise.reject(error);
+      }
     );
   }
 
-  // 封装网络请求的方法
-  request<T = any>(config: RequestConfig<T>) {
-    // 单次请求的成功拦截处理
-    if (config.interceptors?.requestSuccessFn) {
-      config = config.interceptors.requestSuccessFn(config);
-    }
-
-    // 返回Promise
-    return new Promise<T>((resolve, reject) => {
+  request<T = any>(config: RequestConfig<T>): Promise<T> {
+    return new Promise((resolve, reject) => {
       this.instance
         .request<any, T>(config)
-        .then((res) => {
-          // 单次响应的成功拦截处理
-          if (config.interceptors?.responseSuccessFn) {
-            res = config.interceptors.responseSuccessFn(res);
-          }
-          resolve(res);
-        })
-        .catch((err) => {
-          reject(err);
-        });
+        .then((res) => resolve(res))
+        .catch((err) => reject(err));
     });
   }
 
-  get<T = any>(config: RequestConfig<T>) {
+  get<T = any>(config: RequestConfig<T>): Promise<T> {
     return this.request({ ...config, method: 'GET' });
   }
-  post<T = any>(config: RequestConfig<T>) {
+
+  post<T = any>(config: RequestConfig<T>): Promise<T> {
     return this.request({ ...config, method: 'POST' });
   }
-  delete<T = any>(config: RequestConfig<T>) {
+
+  delete<T = any>(config: RequestConfig<T>): Promise<T> {
     return this.request({ ...config, method: 'DELETE' });
   }
-  patch<T = any>(config: RequestConfig<T>) {
+
+  patch<T = any>(config: RequestConfig<T>): Promise<T> {
     return this.request({ ...config, method: 'PATCH' });
   }
 }
--- src/service/request/type.ts
+++ src/service/request/type.ts
@@ -1,8 +1,8 @@
-import type { AxiosRequestConfig, AxiosRequestHeaders, AxiosResponse, AxiosError } from 'axios';
+import type { AxiosRequestConfig, AxiosResponse, AxiosError, InternalAxiosRequestConfig } from 'axios';
 
 // 针对AxiosRequestConfig配置进行扩展
 export interface Interceptors<T = AxiosResponse> {
-  requestSuccessFn?: (config: any) => any;
+  requestSuccessFn?: (config: InternalAxiosRequestConfig) => InternalAxiosRequestConfig;
   requestFailureFn?: (err: AxiosError) => any;
   responseSuccessFn?: (res: T) => T;
   responseFailureFn?: (err: AxiosError) => any;
@@ -10,5 +10,4 @@ export interface Interceptors<T = AxiosResponse> {
 
 export interface RequestConfig<T = AxiosResponse> extends AxiosRequestConfig {
   interceptors?: Interceptors<T>;
-  headers?: AxiosRequestHeaders;
 }
